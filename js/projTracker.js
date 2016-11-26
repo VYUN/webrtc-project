a6bd9b36ee4da362fb817c5bced65d67b97a57c9
Janet Mardjuki <mardjuki@sfu.ca>
2016-11-26 01:54:42 -0800

    Add enable and disable when face detected

---
+++ js/projTracker.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var videoInput = document.getElementById('vid');
+var canvasInput = document.getElementById('compare');
+
+console.log("gg");
+document.addEventListener("headtrackrStatus", function(event) {
+  if (event.status == "found") {
+    console.log("found face");
+    document.getElementById("snap").disabled = false;
+    document.getElementById("send").disabled = false;
+    document.getElementById("snapAndSend").disabled = false;
+  }
+  else {
+    console.log("face unknown");
+    document.getElementById("snap").disabled = true;
+    document.getElementById("send").disabled = true;
+    document.getElementById("snapAndSend").disabled = true;
+  }
+}, true);
+
+var htracker = new headtrackr.Tracker({calcAngles : true, ui : false, headPosition : false});
+htracker.init(videoInput, canvasInput);
+htracker.start();
